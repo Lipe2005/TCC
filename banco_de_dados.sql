bb0a88349f3e176d75e4c99b94ce26ae0ffb1f26
Lipe2005 <92802659+Lipe2005@users.noreply.github.com>
2022-09-14 13:28:07 -0300

    Add files via upload

---
+++ banco_de_dados.sql
@@ -0,0 +1,22 @@
+
+
+CREATE TABLE 'produtos' (
+    `id` int(11) NOT NULL,
+    `tipo` varchar(50) NOT NULL,
+    `marca` varchar(50) NOT NULL,
+)  ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
+INSERT INTO 'produtos' ('id','tipo', 'marca') VALUES
+(1, 'creatina', 'integralmedica')
+(2, 'creatina', 'max titanium')
+(3, 'creatina', 'probiotica');
+(4, 'creatina', 'black skull');
+(5, 'creatina', 'Athletica');
+(6, 'creatina', 'Optimun');
+(7, 'whey', 'integralmedica')
+(8, 'whey', 'max titanium')
+(9, 'whey', 'probiotica');
+(10, 'whey', 'black skull');
+(11, 'whey', 'Athletica');
+(12, 'whey', 'Optimun');
