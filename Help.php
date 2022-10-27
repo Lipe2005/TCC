f8fd5f2956c98836d84bb4dc57d51ef260cb3a05
Lipe2005 <92802659+Lipe2005@users.noreply.github.com>
2022-10-27 13:21:55 -0300

    Add files via upload

---
+++ Help.php
@@ -0,0 +1,179 @@
+<html>
+    <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-COMPATIBLE" content="IE-edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> Pro saude store</title>
+    <link rel="stylesheet" type="text/css" href="D:\3etim2022\TCC\css\TCCstyle.css" />
+    <link rel="icon" href="https://live.staticflickr.com/65535/52294851155_263db00b22_c.jpg" background="none" />
+    <script src="https://kit.fontawesome.com/0342f8c9d6.js" crossorigin="anonymous"></script>
+    <style>
+    * {
+    margin: 0 auto;
+    padding: 0;
+}
+
+body {
+    background-color: #d3e8f7;
+    display: flex;
+}
+
+header {
+    background-color: #2bd9e5;
+    width: 100%;
+    height: 201px;
+}
+
+
+/* Botões */
+
+li {
+    max-width: 2000px;
+    margin: 0;
+    display: flex;
+    font-family: Georgia, serif;
+    font-size: medium;
+    height: 47px;
+    border-bottom: 5px solid #091d29
+}
+
+.button1 {
+    color: #000000;
+    background-color: #2bd9e5;
+    font-size: medium;
+    width: 60px;
+    border: none;
+    margin-top: 10px;
+    margin-left: 5px;
+}
+
+.button2 {
+    color: #000000;
+    background-color: #2bd9e5;
+    font-size: medium;
+    width: 150px;
+    border: none;
+    margin-top: 10px;
+}
+
+.button3 {
+    color: #000000;
+    background-color: #2bd9e5;
+    font-size: medium;
+    width: 90;
+    border: none;
+    margin-top: 10px;
+}
+
+.button4 {
+    color: #000000;
+    background-color: #2bd9e5;
+    font-size: medium;
+    width: 109px;
+    border: none;
+    margin-top: 10px;
+}
+
+.button5 {
+    color: #000000;
+    background-color: #2bd9e5;
+    font-size: medium;
+    width: 70px;
+    border: none;
+    margin-top: 10px;
+}
+
+/* perguntas */
+
+.pergunta{
+    background-color: lightblue;
+    margin-top: 100px;
+    margin-left: 600px;
+    border: solid #091d29 1px;
+}
+
+.um{
+    height: 80px;
+    width: 800px;
+}
+
+.dois{
+    height: 200px;
+    width: 800px;
+}
+
+.tres{
+    height: 110px;
+    width: 800px;
+}
+
+.quatro{
+    height: 140px;
+    width: 800px;
+}
+
+h1{
+    margin-left: 700px;
+    font-size: 45px;
+}
+
+h2{
+    margin-top: 8px;
+    margin-left: 8px;
+}
+
+h3{
+    margin-top: 8px;
+    margin-left: 40px;
+    font-weight: 400;
+}
+
+</style>
+    </head>
+    <Body>
+    <header>
+        <img src="https://live.staticflickr.com/65535/52292209878_faa7da2bd3_b.jpg" width="350" height="150" alt="Pro_saude">
+
+        <nav>
+            <div>
+            <li>
+
+                <input type ="submit" class="button1" value="HOME"
+                        onclick="Location.href='Home.php'">
+
+                <input type ="submit" class="button2" value="PRODUTOS"
+                        onclick="location.href='Produtos.php'">
+
+                <input type ="submit" class="button3" value="CONTA"
+                        onclick="location.href='Login.php'">
+
+                <input type ="submit" class="button2" value="AJUDA"
+                        onclick="location.href='Help.php'">
+
+            </li>
+            </div>
+        <h1>Algumas perguntas frequentes</h1>
+        </nav>
+         <div class="pergunta um">
+            <h2>O que é a Pro-saúde store?</h2>
+            <h3>Nos somos um projeto de tcc feito na etec pedro badran a fim de que a pro-saúde tenha um site</h3>
+         </div>
+         <div class="pergunta dois">
+            <h2>Aonde posso entrar em contrato?</h2>
+            <h3>Você entra em contato pelo número de telefone (16) 2190-1175 ou no nosso instragram @academia_prosaudesjb</h3>
+            <h3>caso queira ir na nossa sede ela se encontra na rua Francisco Aragon 50 no bairro Parque Primavera em São Joaquim da Barra SP-BR</h3>
+         </div>
+         <div class="pergunta tres">
+           <h2>Como fazer uma conta?</h2>
+           <h3>Para isso você clicar no botão superior direito, logo após isso aparecerá uma tela aonde você</h3>
+           <h3>deve colocar o seu e-mail, sua senha e sua idade que por sua vez deverá ser maior de 13 anos</h3>
+         </div>
+         <div class="pergunta quatro">
+         <h2>Como efetuar a compra?</h2>
+            <h3>Para efetuar a compra você precisa ter uma conta, após isso você clica no produto desejado que</h3>
+            <h3>aparecerá uma mensagem perguntando se você realmente quer comprar o produto e colocar o seu</h3>
+            <h3>endereço, após isso é só confirmar o seu pedido</h3>
+         </div>
+    </header>
+    </body>
+</html>
